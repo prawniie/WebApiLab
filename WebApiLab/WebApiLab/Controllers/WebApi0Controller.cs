7f837a44ae0ba2fc8f72c6b533b6241ec6d6edc0
rebeckalscarlsson <rebecka.ls.carlsson@gmail.com>
2019-01-28 11:08:33 +0100

    Initial commit

---
+++ WebApiLab/WebApiLab/Controllers/WebApi0Controller.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiLab.Controllers
+{
+    [Route("webapi0")]
+    public class WebApi0Controller : Controller
+    {
+        [Route("kalle")]
+        public string Kalle()
+        {
+            return "Hej från servern!";
+        }
+
+        [Route("kalle2")]
+        public string Kalle2()
+        {
+            return "Klockan är " + DateTime.Now;
+        }
+
+        [Route("kalle3")]
+        public int Kalle3()
+        {
+            int i = 10 + 32;
+            return i;
+        }
+
+        [Route("kalle4"), HttpGet] //Kan användas för att se till att man bara blir insläptt om man vill göra post
+        [HttpGet("kalle4")] //Samma sak som det ovan
+        public IActionResult Kalle4()
+        {
+            //return BadRequest(); //tex om användaren matar in epostadress som inte finns
+            //return NotFound();
+            return Ok("Klockan är " + DateTime.Now); //Ok betyder att allt gick bra, i Ok kan man skicka in typ vad som helst
+        }
+
+    }
+}
---
+++ WebApiLab/WebApiLab/Controllers/WebApi1Controller.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiLab.Models;
+
+namespace WebApiLab.Controllers
+{
+    [Route("webapi1")]
+    public class WebApi1Controller : Controller
+    {
+        [Route("AddPlanet")]
+        public IActionResult AddPlanet()
+        {
+            string formContent = "";
+            using(StreamReader reader = new StreamReader(Request.Body, Encoding.UTF8))
+            {
+                formContent = reader.ReadToEndAsync().Result;
+            }
+
+            Planet planet = ParsePlanet(formContent);
+
+            return Ok("Ny planet " + planet.Name + " skapad med storleken " + planet.Size.ToString());
+        }
+
+        private Planet ParsePlanet(string formContent)
+        {
+            //Output: Planet=Pluto&Size=12
+
+            string[] formContentArray = formContent.Split('&');
+
+            //Nackdel med denna lösning då den inte funkar om man ändrar i formuläret eller byter plats på grejerna tex
+            Planet planet = new Planet
+            {
+                Name = formContentArray[0].Remove(0, 7),
+                Size = int.Parse(formContentArray[1].Remove(0, 5))
+            };
+
+            return planet;
+        }
+
+        [Route("GetPlanet")]
+        public IActionResult GetPlanet()
+        {
+
+            //Output: "?Planet=Mars&Size=125"
+            string[] queryStringArray = Request.QueryString.Value.Split('&');
+
+            Planet planet = new Planet
+            {
+                Name = queryStringArray[0].Remove(0, 8),
+                Size = int.Parse(queryStringArray[1].Remove(0, 5))
+            };
+
+
+            return Ok("Söker i databasen efter planeter med namn " + planet.Name + " och storlek " + planet.Size.ToString());
+        }
+    }
+}
